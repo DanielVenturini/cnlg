34b31ca19a38391894b944c75d406a1d53468b11
Daniel Venturini <danielventurini021@gmail.com>
2019-06-15 22:25:53 -0300

    Add break line to separed objects and try..catch

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ function vars(argsKeys) {
 }
 
 module.exports = function() {
-  // when calls cnlg({var1, var2})
-  // the Node.js converts {var1, var2} in {'0': {var1: 'value', var2: 'value2'}}
-  for(var mainKey in Object.keys(arguments)) {
-    vars(arguments[mainKey])
+  try {
+    // when calls cnlg({var1, var2})
+    // the Node.js converts {var1, var2} in {'0': {var1: 'value', var2: 'value2'}}
+    for(var mainKey in Object.keys(arguments)) {
+      vars(arguments[mainKey])
+      console.log()   // break line to more than one object: cnlg({k, b, g}, {k, b})
+    }
+  } catch (e) {
+    console.log('Wrong call. Please, use \'cnlg({obj1, obj2})\'')
   }
 }
